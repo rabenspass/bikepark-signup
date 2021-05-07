7196248f5367e5cf1d40553c8c145fe329a56f90
Thies Mueller <git@td00.de>
2021-05-07 23:51:23 +0200

    hochkommata sind hass

--- adminarea_sessions.php
+++ adminarea_sessions.php
@@ -31,7 +31,7 @@ if(mysqli_connect_errno()){
 }
 
 //get results from database
-$result = mysqli_query($connection,"SELECT * FROM users WHERE profilepicture="3");
+$result = mysqli_query($connection,'SELECT * FROM users WHERE profilepicture="3"');
 $all_property = array();  //declare an array for saving property
 
 //showing property
